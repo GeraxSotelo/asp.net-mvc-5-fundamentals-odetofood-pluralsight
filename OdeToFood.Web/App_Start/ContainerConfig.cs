ec3077708df1ffe93c0351730171ccb279221929
Gerax Sotelo <gsotelo23@gmail.com>
2020-04-08 17:07:02 -0600

    Add ContainerConfig class & register in Global.asx. Build Autofac container & set as dependency resolver

---
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace OdeToFood.Web
+{
+    //register in Global.asax.cs
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            //from Global.asax.cs
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            //register type InMemoryRestaurantData and use it whenever someone asks for an object that implements IRestaurantData
+            builder.RegisterType<InMemoryRestaurantData>().As<IRestaurantData>().SingleInstance();
+
+            //create an Autofac container from the container builder to tell the MVC5 framework to use this container whenever it needs to resolve dependencies
+            var container = builder.Build();
+            //set container as the dependency resolver throughout the running of this MVC5 application
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
